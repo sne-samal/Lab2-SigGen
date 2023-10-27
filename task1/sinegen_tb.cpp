2b3e477cd627a00e5dd8fa5f62219cc6f4d75491
sne-samal <sne.samal@gmail.com>
2023-10-27 18:15:39 +0100

    Added some task 1 stuff

---
+++ task1/sinegen_tb.cpp
@@ -0,0 +1,49 @@
+#include "verilated.h"
+#include "verilated_vcd_c.h"
+#include "Vsinegen.h"
+#include "vbuddy.cpp"
+
+int main(int argc, char **argv, char **env) {
+    int i; // clock count
+    int tick; // 
+
+    Verilated::commandArgs(argc, argv);
+    // init top verilog instance
+    Vsinegen* top = new Vsinegen;
+
+    // init trace dump
+    Verilated::traceEverOn(true);
+    VerilatedVcdC* tfp = new VerilatedVcdC;
+    top->trace (tfp, 99);
+    tfp->open("sinegen.vcd");
+
+    top->clk = 1;
+    top->rst = 0;
+    top->en = 1;
+    top->incr = 1;
+
+    if (vbdOpen()!=1) return(-1);
+    vbdHeader("L2T1: SigGen");
+
+    for (i=0; i < 1000000; i++)
+    {
+
+        for (tick=0; tick<2; tick++) {
+            tfp->dump (2*i+tick);
+            top->clk = !top->clk;
+            top->eval ();
+        }
+        
+        
+
+        vbdPlot(int (top->dout), 0, 255);
+        vbdCycle(i);
+
+        if ((Verilated::gotFinish()) || (vbdGetkey()=='q')) 
+            exit(0); 
+    }
+
+    vbdClose();   
+    tfp->close(); 
+    exit(0);
+}
